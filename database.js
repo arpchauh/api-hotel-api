d5e85a150c5f9644db48c82a3ac6c4759df9f384
arpchauh <arpitchauhan10@gmail.com>
2020-04-07 21:27:47 +0530

    init

--- database.js
+++ database.js
@@ -1,22 +1,11 @@
-// init code
-
 const mongoose = require("mongoose");
-
-const db_url = process.env.DB_URL;
 
 //connection code
 mongoose
-  .connect("mongodb://127.0.0.1:27017/hotel_backend", { useNewUrlParser: true })
+  .connect("mongodb://arpit:arpit123@ds127139.mlab.com:27139/malvan_hotel", {
+    useNewUrlParser: true
+  })
   .then(console.log("Mongoose connected"))
   .catch(error => {
     console.log(error);
   });
-let db = mongoose.connection;
-
-db.once("open", function() {
-  console.log("Connected to MongoDB");
-});
-
-db.on("error", function(err) {
-  console.log(err);
-});
