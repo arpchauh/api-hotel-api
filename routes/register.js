7961a369782f549760349f749a33903f07536bad
arpchauh <arpitchauhan10@gmail.com>
2020-04-08 19:02:16 +0530

    get api created

--- routes/register.js
+++ routes/register.js
@@ -65,7 +65,18 @@ router.delete("/:userId", (req, res, next) => {
 });
 
 router.get("/user", (req, res, next) => {
-  User.find().exec().then().catch();
+  User.find({})
+    .exec()
+    .then((result) => {
+      res.status(200).json({
+        userDetails: result,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+      });
+    });
 });
 
 module.exports = router;
